ce9396fe2fbc7170507da81ead677bd8e662bcc0
Nick <mkz_hychen@sina.com>
2018-09-23 16:12:43 +0800

    [ADD] springBoot 内置容器的切换，以及tomcat的代码配置

--- src/main/java/com/nick/MySpringBootApplication.java
+++ src/main/java/com/nick/MySpringBootApplication.java
@@ -2,11 +2,16 @@ package com.nick;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.embedded.EmbeddedWebServerFactoryCustomizerAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.servlet.TomcatWebSocketServletWebServerCustomizer;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
 
 import java.util.concurrent.Executor;
 
@@ -33,5 +38,17 @@ public class MySpringBootApplication extends AsyncConfigurerSupport {
 		executor.setThreadNamePrefix("Async-");
 		executor.initialize();
 		return executor;
+	}
+
+	/**
+	 * 通过代码配置内置servlet容器
+	 */
+	@Component
+	public static class CustomServletContainer implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+		@Override
+		public void customize(ConfigurableServletWebServerFactory configurableServletWebServerFactory) {
+			configurableServletWebServerFactory.setContextPath("/nick"); //相当于yml中的server.context-path: /nick
+			configurableServletWebServerFactory.setPort(8012); //相当于yml中的server.port: 8012
+		}
 	}
 }
